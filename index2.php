771e987e327bc5495c69579b5270a6bcc3db0843
root <root@localhost.localdomain>
2020-04-25 12:02:51 +0900

    생성 - 메인페이지 상단 메뉴

---
+++ index2.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+
+  <body bgcolor="#79ABFF">
+    <?php
+      echo $_POST['id'].'님 반갑습니다';
+    ?>
+  </body>
+</html>
