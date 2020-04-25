cb6d9042cd4326a433638198ee4c3ef67856f82c
root <root@localhost.localdomain>
2020-04-25 21:23:20 +0900

    추가 - 커뮤니티페이지에서 데이터베이스로 입력값 전달기능

--- index2.php
+++ index2.php
@@ -5,8 +5,27 @@
   </head>
 
   <body bgcolor="#79ABFF">
-    <?php
-      echo $_POST['id'].'님 반갑습니다';
-    ?>
+  <?php
+  $mail = $_POST['mail'];
+  $name = $_POST['name'];
+  $memo = $_POST['memo'];
+
+  echo $date = date("YmdHis", time());
+  echo "<br>";
+  echo $ip=getenv("REMOTE_ADDR");
+
+   $connect = mysqli_connect("localhost:3306", "root", "x#eYb@jHlGZErzWtKpeH", "beenDB"); //mysql 연결
+   if (!$connect) {
+     echo "연결에 실패 하였습니다.".mysqli_error();
+   }
+
+   //쿼리전송
+   $query = "insert into community(mail, name, memo, date, ip) 
+                           values('$mail', '$name', '$memo', '$date', '$ip')";
+
+   mysqli_query($connect, $query);
+
+   mysqli_close($connect);
+  ?>
   </body>
 </html>
