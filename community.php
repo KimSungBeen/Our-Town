64aaa37cae1f92e018ed1fb84dce74982f522a12
root <root@localhost.localdomain>
2020-04-25 22:24:07 +0900

    생성 - 데이터베이스에서 작성한 글 불러오기

--- community.php
+++ community.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,7 +6,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>커뮤니티</title>
 </head>
-<body>
+<body bgcolor="#79ABFF">
     
+<?php
+    $connect = mysqli_connect("localhost:3306", "root", "x#eYb@jHlGZErzWtKpeH", "beenDB"); //DB연결
+    if(!$connect)die("연결에 실패 하였습니다.".mysqli_error());
+
+    //쿼리문으로 데이터 불러오기
+    $query = "select * from community";
+    $result = mysqli_query($connect, $query);
+
+    //데이터 출력하기
+    while($data = mysqli_fetch_array($result)) {
+    
+    echo $data['no'].'. ';
+    echo '메모: '.$data['memo'];
+    echo '이름: '.$data['name'];
+    echo '이메일: '.$data['mail'].'<br>';
+    }
+?>
+
 </body>
 </html>
